e0242c2dbbc4f3b450ee56ebcf99a747eeef3502
thor <thor.schott@gmail.com>
2026-09-22 00:35:34 +0200

    Use Firebase for instructor login and isolate class switching

---
+++ platform/firebase-login.js
@@ -0,0 +1,15 @@
+import {initializeApp} from 'https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js';
+import {getAuth, GithubAuthProvider, signInWithPopup, setPersistence, browserLocalPersistence} from 'https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js';
+
+const app=initializeApp({
+  apiKey:'AIzaSyDTcGhpCiH5DvgxBqEZslhFnpAf8ULqFwI',
+  authDomain:'thorscorsv04.firebaseapp.com',
+  projectId:'thorscorsv04',
+  appId:'1:655007055435:web:9f589f2527adc9587997ce'
+});
+const auth=getAuth(app);
+export async function githubToken(){
+  await setPersistence(auth,browserLocalPersistence);
+  const result=await signInWithPopup(auth,new GithubAuthProvider());
+  return result.user.getIdToken(true);
+}
